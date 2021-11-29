32f8c5451f98a47f2f7eeff0b7e4b2f044470cdc
Matthew Elphick <matthew.elphick@threadsstyling.com>
2021-11-29 11:44:13 +0000

    feat: 🎸 Add minimal service worker

---
+++ public/sw.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-undef */
+self.addEventListener('install', function (_event) {
+  console.log('Hello world from the Service Worker 🚀')
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -78,6 +78,18 @@ function Footer() {
 
 function App({ Component, pageProps }: AppProps) {
   useFathom()
+  React.useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').then(
+        function (registration) {
+          console.log('Service Worker registration successful with scope: ', registration.scope)
+        },
+        function (err) {
+          console.log('Service Worker registration failed: ', err)
+        }
+      )
+    }
+  }, [])
   return (
     <>
       <Head>
